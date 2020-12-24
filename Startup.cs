a915a89c7bbbe9646e358d82b1ee11675e04eb78
CJ Managuelod <cjmanaguelod1895@outlook.com>
2020-12-24 12:54:26 +0800

    Remove Inventory-Web-API.xml

--- Startup.cs
+++ Startup.cs
@@ -66,9 +66,9 @@ namespace Inventory_Web_API
                 });
 
                 // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
             });
             #endregion
 
